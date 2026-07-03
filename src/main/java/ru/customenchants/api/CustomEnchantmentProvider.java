155b72af3bff2f602fd5f88d1295dfbf17fc42c2
alex502gg-dotcom <alex502gg@gmail.com>
2026-07-04 01:22:29 +0500

    Delete src directory

--- src/main/java/ru/customenchants/api/CustomEnchantmentProvider.java
+++
@@ -1,9 +0,0 @@
-package ru.customenchants.api;
-
-import org.bukkit.plugin.Plugin;
-
-import java.util.Collection;
-
-public interface CustomEnchantmentProvider {
-    Collection<CustomEnchantment> createEnchantments(Plugin plugin);
-}
